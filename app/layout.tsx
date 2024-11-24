5a259d617705c19f14a2da876670c7e6d4ff4b06
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2024-11-24 02:34:30 +0000

    feat: initial page (#1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import PlausibleProvider from "next-plausible";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <PlausibleProvider
+          domain="nunom.dev"
+          customDomain="https://plausible.home.nunom.dev"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
