bdad5d0cd859943c4d10b8e42e58739068713cad
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2026-02-17 19:52:19 +0000

    feat: migrate to react router and add experience sections (#4)

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const { withPlausibleProxy } = require("next-plausible");
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
